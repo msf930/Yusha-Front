b57128843098fb8edefd91e7e36caadef85a5732
mfolsom930 <86228082+mfolsom930@users.noreply.github.com>
2023-07-03 16:46:10 -0600

    second commit

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
-const cors = require('cors')
+
 
 const app = express()
 
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
     next()
 })
 
-app.use(cors())
+
 app.use('/api/workouts', workoutRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
