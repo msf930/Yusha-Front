1d4ae45dced5ad0859f84f585aee9e6440f068a6
CJ <cjfritz9@gmail.com>
2023-07-06 11:56:14 -0600

    react build file serving with node added

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,40 @@
-require('dotenv').config()
+require('dotenv').config();
+const path = require("path");
 
-const express = require('express')
-const mongoose = require('mongoose')
-const workoutRoutes = require('./routes/workouts')
+const express = require('express');
+const mongoose = require('mongoose');
+const workoutRoutes = require('./routes/workouts');
 
+const app = express();
 
-const app = express()
-
-app.use(express.json())
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
+app.use('/api/workouts', workoutRoutes);
 
-app.use('/api/workouts', workoutRoutes)
+app.get('/*', (_req, res) => {
+  res.sendFile(path.join(path.join(__dirname, 'public', 'index.html')));
+});
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        // listen to port
-        if(process.env.PORT) {
-            app.listen(process.env.PORT, () => {
-                console.log('connected to DB and listening for requests on port', process.env.PORT)
-            })
-        }
-        module.exports = app;
-    })
-    .catch((err) => {
-        console.log(err)
-    })
-
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen to port
+    if (process.env.PORT) {
+      app.listen(process.env.PORT, () => {
+        console.log(
+          'connected to DB and listening for requests on port',
+          process.env.PORT
+        );
+      });
+    }
+    module.exports = app;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
